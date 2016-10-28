3e0179444b299cde090f9551609e2f8ac47e189f
tsarachan <tes353@nyu.edu>
2016-10-28 16:17:49 -0400

    Enemies spawn based on time
    SpawnEnemies creates enemies in the way the text file would, just in a much less convenient way!

---
+++ 3D Hot Potato/Assets/Scripts/SpawnEnemies.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpawnEnemies : MonoBehaviour {
+
+	private List<Transform> spawners = new List<Transform>();
+
+	[Header("When enemies should spawn")]
+	public float[] spawnTimes;
+	[Header("Which enemy prefab to spawn")]
+	public GameObject[] enemyToSpawn;
+	[Header("Which spawner--1-5, left to right")]
+	public int[] whichSpawner;
+	private int index = 0;
+	private float timer = 0.0f;
+
+	private void Start(){
+		spawners = FindSpawners();
+	}
+
+	private List<Transform> FindSpawners(){
+		List<Transform> temp = new List<Transform>();
+
+		foreach (Transform spawner in transform){
+			temp.Add(spawner);
+		}
+
+		if (temp.Count == 0) { Debug.Log("Couldn't find any spawners!"); }
+
+		return temp;
+	}
+
+	private void Update(){
+		timer += Time.deltaTime;
+
+
+		if (index <= spawnTimes.Length - 1 && index <= enemyToSpawn.Length - 1 && index <= whichSpawner.Length - 1){
+			if (timer >= spawnTimes[index]){
+				GameObject newEnemy = Instantiate(enemyToSpawn[index], spawners[whichSpawner[index] - 1].position, Quaternion.identity) 
+					as GameObject;
+				index++;
+			}
+		}
+	}
+}
