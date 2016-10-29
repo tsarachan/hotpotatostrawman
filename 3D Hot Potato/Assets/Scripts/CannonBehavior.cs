97008e36f5c7abce4a77352223b55c4a8b7f4fd7
tsarachan <tes353@nyu.edu>
2016-10-28 22:20:40 -0400

    Cannon fires cannonballs

--- 3D Hot Potato/Assets/Scripts/CannonBehavior.cs
+++ 3D Hot Potato/Assets/Scripts/CannonBehavior.cs
@@ -11,9 +11,17 @@ public class CannonBehavior : MonoBehaviour {
 	public float speed = 1.0f;
 	public float range = 1.0f;
 
+	private const string BALL_OBJ = "Ball";
+	private const string CANNONBALL_OBJ = "Cannonball";
+	private GameObject cannonball;
+	private bool reloading = false;
+	public float reloadTime = 1.0f; //how long the players have to wait between attacks
+	private float timer = 0.0f;
+
 	private void Start(){
 		rb = GetComponent<Rigidbody>();
 		startZ = transform.localPosition.z;
+		cannonball = Resources.Load(CANNONBALL_OBJ) as GameObject;
 	}
 
 	//The boss moves left and right.
@@ -41,5 +49,51 @@ public class CannonBehavior : MonoBehaviour {
 		float z = transform.parent.position.z + startZ + range * Mathf.Sin(Time.time * speed);
 
 		return z;
+	}
+
+	/// <summary>
+	/// Shoot the cannon when the players hit it with the ball, if it's not reloading.
+	/// </summary>
+	/// <param name="other">The collider that's hit the cannon.</param>
+	private void OnTriggerEnter(Collider other){
+		if (other.gameObject.name.Contains(BALL_OBJ) && !reloading){
+			reloading = Shoot();
+		}
+	}
+
+	/// <summary>
+	/// Create a cannonball and set the cannon to reloading.
+	/// 
+	/// This doesn't tell the cannonball what to do; that's the cannonball's responsibility.
+	/// </summary>
+	private bool Shoot(){
+		Instantiate(cannonball, transform.position, Quaternion.identity);
+
+		return true;
+	}
+
+	/// <summary>
+	/// If the cannon is reloading, run the timer until it's reloaded.
+	/// </summary>
+	private void Update(){
+		if (reloading){
+			reloading = RunReloadTimer();
+		}
+	}
+
+
+	/// <summary>
+	/// Increments the reload timer until the cannon has reloaded.
+	/// </summary>
+	/// <returns><c>true</c> if the reload time is complete <c>false</c> otherwise.</returns>
+	private bool RunReloadTimer(){
+		timer += Time.deltaTime;
+
+		if (timer >= reloadTime){
+			timer = 0.0f;
+			return false;
+		} else {
+			return true;
+		}
 	}
 }
---
+++ 3D Hot Potato/Assets/Scripts/CannonballBehavior.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CannonballBehavior : MonoBehaviour {
+
+	public float speed = 20.0f;
+
+	private Rigidbody rb;
+
+	private const string BOSS_OBJ = "Boss";
+
+	private void Start(){
+		rb = GetComponent<Rigidbody>();
+		rb.AddForce(new Vector3(0.0f, 0.0f, speed), ForceMode.Impulse); //move straight up
+	}
+
+	private void OnTriggerEnter(Collider other){
+		if (other.name.Contains(BOSS_OBJ)){
+			other.GetComponent<EnemyBoss>().GetHit();
+			Destroy(gameObject);
+		}
+	}
+}
