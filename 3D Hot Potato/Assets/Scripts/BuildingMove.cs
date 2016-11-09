6423ff65eb7d3e63f7fd150505c498c023fd622b
dannynanni <dannynanni.us@gmail.com>
2016-11-09 12:36:18 -0500

    added neon package; changed some graphics; still working

---
+++ 3D Hot Potato/Assets/Scripts/BuildingMove.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildingMove : MonoBehaviour {
+
+	public float speed;
+	bool gameStart;
+	Vector3 startPos;
+
+	void Start(){
+
+	
+	}
+	void Update(){
+		transform.localPosition -= transform.forward * 50 * Time.deltaTime;
+
+		if (transform.localPosition.z == -25) {
+		
+			transform.localPosition = new Vector3 (-34.9f, 9.04f, 74.8f);
+		}
+
+	}
+}
