a7af3e7273bc0a8d12b08be1b720fa0a076f72a0
tsarachan <tes353@nyu.edu>
2016-11-10 14:03:11 -0500

    Reading from JSON
    Level information can now be read from a JSON file.

---
+++ 3D Hot Potato/Assets/Scripts/FileIOUtil.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using SimpleJSON;
+
+public class FileIOUtil {
+
+	public static void WriteJsonToFile(string fileName, JSONClass json)
+	{
+		string output = json.ToString();
+
+		WriteStringToFile(fileName, output, false);
+	}
+
+	public static JSONNode ReadJsonFromFile(string fileName)
+	{
+		if (Path.GetExtension(fileName) != ".json")
+		{
+			return null;
+		}
+
+		string input = ReadStringFromFile(fileName);
+
+		JSONNode converted = JSONClass.Parse(input);
+
+		return converted;
+	}
+
+	/// <summary>
+	/// Write a string to a file
+	/// </summary>
+	/// <param name="fileName">File to write to.</param>
+	/// <param name="content">String to write.</param>
+	/// <param name="append">If set to <c>true</c>, append. If set to <c>false<c>, overwrite file.</param>
+	public static void WriteStringToFile(string fileName, string content, bool append)
+	{
+		StreamWriter sw = new StreamWriter(fileName, append);
+
+		sw.WriteLine(content);
+
+		sw.Close();
+	}
+
+	/// <summary>
+	/// Reads a string from a file.
+	/// </summary>
+	/// <returns>The string from file. If file does not exist, returns null.</returns>
+	/// <param name="fileName">Name of file to read from.</param>
+	public static string ReadStringFromFile(string fileName)
+	{
+		if (File.Exists(fileName))
+		{
+			StreamReader sr = new StreamReader(fileName);
+
+			string input = sr.ReadToEnd();
+
+			sr.Close();
+
+			return input;
+		}
+		else
+		{
+			Debug.Log("Warning: attempted to read from \\" + fileName + ", file does not exist");
+			return null;
+		}
+	}
+
+	/// <summary>
+	/// Creates an array of the items in a string, separating by a character provided as an argument.
+	/// </summary>
+	/// <returns>A string array.</returns>
+	/// <param name="fileName">Name of file to read from.</param>
+	/// <param name="separator">The character to use as a separator.</param>
+	public static string[] SplitStringArrayFromFile(string filename, char separator)
+	{
+		string unsplitString = FileIOUtil.ReadStringFromFile(filename);
+		
+		return unsplitString.Split(new char[] { separator });
+	}
+
+	/// <summary>
+	/// Turns a JSON node into a 1-dimensional string.
+	/// </summary>
+	/// <returns>The 1D string.</returns>
+	/// <param name="retrieveInfo">Name of the JSON node to read.</param>
+	/// <param name="filePath">File path.</param>
+	/// <param name="fileName">File name.</param>
+	public static string GetFromFile(string retrieveInfo, string filePath, string fileName)
+	{
+		JSONNode jsonLevel = FileIOUtil.ReadJsonFromFile(Application.dataPath + filePath + fileName);
+		string readString = jsonLevel[retrieveInfo].ToString();
+		readString = readString.Substring(1); //discard initial quotation mark that is part of JSON formatting
+		readString = readString.TrimEnd('"');
+		return readString;
+	}
+}
---
+++ 3D Hot Potato/Assets/Scripts/LoadFromJSon.cs
@@ -0,0 +1,62 @@
+﻿/*
+ * 
+ * This script loads enemies and level elements from a JSon file.
+ * 
+ */
+
+using SimpleJSON;
+using UnityEngine;
+using System.Collections;
+
+public class LoadFromJSon : MonoBehaviour {
+
+	public class JSonEntry {
+
+	}
+
+	//names of JSon objects--these are used to read lines from the JSON file
+	private const string LEVELS = "levels";
+	private const string LEVEL = "level";
+	private const string ENEMIES_TO_MAKE = "enemiesToMake";
+	private int levelNumber = 1; //which level are we on? Starts at level 1.
+	private const string SPAWN = "spawn";
+	private const string WHICH_ENEMY = "whichEnemy";
+	private const string WAVES = "waves";
+	private const string WHICH_SPAWNERS = "whichSpawners";
+	private const string TIME_BETWEEN_WAVES = "timeBetweenWaves";
+	private const string TIME_VARIANCE = "timeVariance";
+
+	private float timer = 0.0f; //the time the players have spent in the level so far
+	private float nextReadTime = 0.0f; //when should the system read the next item from the JSon file?
+	private int readIndex = 0; //the next thing to read in the "enemiesToMake" JSon object for the current level
+
+	//these are used to find the JSon file in the file directory
+	private const string LEVEL_PATH = "/Levels/";
+	private const string FILE_NAME = "Levels.json";
+
+	private void Start(){
+		//begin a level by reading the first JSon object
+		readIndex = ReadInItem();
+	}
+
+	private void Update(){
+		timer += Time.deltaTime;
+	}
+
+	public int ReadInItem(){
+		JSONNode node = JSON.Parse(FileIOUtil.ReadStringFromFile(Application.dataPath + LEVEL_PATH + FILE_NAME));
+//		Debug.Log(node);
+//		Debug.Log(node[LEVELS][LEVEL + "1"][ENEMIES_TO_MAKE]);
+//		Debug.Log(node[LEVELS][LEVEL + "1"][ENEMIES_TO_MAKE][0]);
+//		Debug.Log(node[LEVELS][LEVEL + "1"][ENEMIES_TO_MAKE][0]["spawn"]);
+//		Debug.Log(node[LEVELS][LEVEL + levelNumber.ToString()][ENEMIES_TO_MAKE][0]["spawn"].AsInt);
+
+		int numWaves = node[LEVELS][LEVEL + levelNumber.ToString()][ENEMIES_TO_MAKE][readIndex][WAVES].AsInt;
+
+		Debug.Log(numWaves);
+
+		readIndex++;
+
+		return readIndex;
+	}
+}
