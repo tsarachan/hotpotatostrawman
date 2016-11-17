b1dcc2b966f180859e5358d436e959e2d0f8ca98
Zach <improvisedphilosophy@gmail.com>
2016-11-17 14:34:46 -0500

    Added ForceSpawner

--- 3D Hot Potato/Assets/Scripts/ForceSpawn.cs
+++ 3D Hot Potato/Assets/Scripts/ForceSpawn.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 
 public class ForceSpawn : MonoBehaviour {
 
+	[Header("Which enemies can you spawn?")]
+	public GameObject[] enemyToSpawn;
+
 	private List<Transform> spawners = new List<Transform>();
 
 	private void Start(){
@@ -24,5 +27,31 @@ public class ForceSpawn : MonoBehaviour {
 
 	private void Update(){
 
+		//spawn a chaser at a random spawner
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			Debug.Log("Finding a random spawner");
+//			select a random spawner
+//			Debug.Log("Number of spawners: " + spawners.Count);
+			Transform randomSpawnerTransform = spawners[Random.Range(0, spawners.Count)];
+			Debug.Log ("Got a spawner");
+//			Transform randomSpawnerTransform = spawners[Random.Range(0, spawners.Count)];
+			//instantiate a chaser there
+			GameObject newEnemy = Instantiate (enemyToSpawn [0],
+				                      randomSpawnerTransform.position,
+				                      enemyToSpawn [0].transform.rotation) as GameObject;
+		}
+
+		if (Input.GetKeyDown (KeyCode.Alpha2)) {
+			Debug.Log("Finding a random side spawner");
+			//			select a random spawner
+			//			Debug.Log("Number of spawners: " + spawners.Count);
+			Transform randomSpawnerTransform = spawners[Random.Range(7, 12)];
+			Debug.Log ("Got a spawner");
+			//			Transform randomSpawnerTransform = spawners[Random.Range(0, spawners.Count)];
+			//instantiate a chaser there
+			GameObject newEnemy = Instantiate (enemyToSpawn [0],
+				randomSpawnerTransform.position,
+				enemyToSpawn [0].transform.rotation) as GameObject;
+		}
 	}
 }
