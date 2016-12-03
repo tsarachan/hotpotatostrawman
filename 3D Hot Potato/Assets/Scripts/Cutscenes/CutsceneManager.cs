ed23e3fdff7f580457146f15ea3e46f723f255f0
tsarachan <tes353@nyu.edu>
2016-12-03 15:24:47 -0500

    P2 dodges enemy
    An enemy appears during the cutscene, and player 2 dodges it.

--- 3D Hot Potato/Assets/Scripts/Cutscenes/CutsceneManager.cs
+++ 3D Hot Potato/Assets/Scripts/Cutscenes/CutsceneManager.cs
@@ -17,25 +17,47 @@ namespace Cutscene
 		//these are the various scripts used in the cutscene
 		private CameraMove cameraMoveScript;
 		private const string CAMERA_BOOM = "Camera boom";
+		private EnemyDirected enemyDirected1;
+		private const string ENEMY_1 = "CutsceneEnemy 1";
+		private PlayerMovement p2MovementScript;
+		private const string P2 = "Player 2";
 
 		//these variables are used to manage when each event occurs
 		private float timer = 0.0f; //how long the cutscene has run; this is the master timer that controls when things happen
 		private List<float> eventTimes = new List<float>(); //all the times when this script will send an instruction to another script
 		private int index = 0; //which event are we on? Zero-indexed to work with eventTimes.
 
+		//these match the directions in InputManager; they're used to move the players during the cutscene
+		private const string UP = "up";
+		private const string DOWN = "down";
+		private const string LEFT = "left";
+		private const string RIGHT = "right";
+
 
 		//everything below this is "direction" from the director
 
-		//pull the camera back to show the players
 		[Header("Move the camera back to show players")]
 		public float moveBackStartTime = 0.0f;
 		public float moveBackDuration = 2.0f;
 		public Vector3 moveBackToPos = new Vector3(0.0f, 0.0f, 0.0f);
 		public AnimationCurve moveBackCurve;
 
+		[Header("An enemy chases P2 from behind")]
+		public float enemyMoveStartTime = 0.0f;
+		public float enemyMoveDuration = 2.0f;
+		public Vector3 enemyMoveToPos = new Vector3(-11.9f, 0.0f, 35.62f);
+		public AnimationCurve enemyMoveCurve;
+
+		[Header("P2 dodges")]
+		public float p2MoveStartTime = 0.0f;
+		public float p2MoveDuration = 1.0f;
+
+
 
 		private void Start(){
 			cameraMoveScript = GameObject.Find(CAMERA_BOOM).GetComponent<CameraMove>();
+			enemyDirected1 = GameObject.Find(ENEMY_1).GetComponent<EnemyDirected>();
+			p2MovementScript = GameObject.Find(P2).GetComponent<PlayerMovement>();
 			eventTimes = GetEventTimes();
 		}
 
@@ -43,11 +65,15 @@ namespace Cutscene
 			List<float> temp = new List<float>();
 
 			temp.Add(moveBackStartTime);
+			temp.Add(enemyMoveStartTime);
+			temp.Add(p2MoveStartTime);
 
 			return temp;
 		}
 
 		private void Update(){
+			Debug.DrawLine(enemyDirected1.transform.position, enemyMoveToPos, Color.red, 2.0f);
+
 			if (index < eventTimes.Count){ //stop trying to do things once the cutscene is complete
 				timer += Time.deltaTime;
 
@@ -62,13 +88,32 @@ namespace Cutscene
 				case 0:
 					cameraMoveScript.MoveBoom(moveBackToPos, moveBackDuration, moveBackCurve);
 					break;
-
+				case 1:
+					enemyDirected1.MoveEnemy(enemyMoveToPos, enemyMoveDuration, enemyMoveCurve);
+					break;
+				case 2:
+					StartCoroutine(MovePlayer(p2MovementScript, p2MoveDuration));
+					break;
 			}
 
 
 			index ++;
 
 			return index;
+		}
+
+		private IEnumerator MovePlayer(PlayerMovement moveScript, float duration){
+			float moveTimer = 0.0f;
+			while(moveTimer <= duration){
+				moveScript.Move(DOWN);
+				moveScript.Move(RIGHT);
+
+				moveTimer += Time.deltaTime;
+
+				yield return null;
+			}
+
+			yield break;
 		}
 	}
 }
---
+++ 3D Hot Potato/Assets/Scripts/Cutscenes/EnemyDirected.cs
@@ -0,0 +1,61 @@
+﻿/*
+ * 
+ * This script moves an enemy in a "directed" way for cutscenes--it does what it's told, without any independent behavior.
+ * 
+ * The setup here assumes that the enemy will be able to complete one move before being told the next thing it needs to do.
+ * It can't queue up moves to zig-zag or the like; it must receive the zig and finish it before being told to zag.
+ * 
+ */
+
+namespace Cutscene
+{
+	using UnityEngine;
+	using System.Collections;
+
+	public class EnemyDirected : EnemyBase {
+
+		private float timer = 0.0f;
+
+		//variables controlling this enemy's movement
+		private float startMoveTime = 0.0f;
+		private float endMoveTime = 0.0f;
+		private Vector3 startMovePos = Vector3.zero;
+		private Vector3 endMovePos = Vector3.zero;
+		private AnimationCurve moveCurve;
+
+
+
+		private void Update(){
+			timer += Time.deltaTime;
+
+			if (timer >= startMoveTime && timer <= endMoveTime){
+				transform.position = MoveTransform();
+			}
+		}
+
+		/// <summary>
+		/// Moves this enemy. The values used are set in MoveEnemy(), below.
+		/// </summary>
+		/// <returns>The enemy's new position.</returns>
+		private Vector3 MoveTransform(){
+			return Vector3.Lerp(startMovePos,
+								endMovePos,
+								moveCurve.Evaluate((timer - startMoveTime)/(endMoveTime - startMoveTime)));
+		}
+
+
+		/// <summary>
+		/// CutsceneManager calls this to set this enemy in motion. All values MoveTransform() needs are set here.
+		/// </summary>
+		/// <param name="endPos">The enemy's final position.</param>
+		/// <param name="moveTime">How long the enemy takes to move.</param>
+		/// <param name="curve">An animation curve to fine-tune the move's speed.</param>
+		public void MoveEnemy(Vector3 endPos, float moveTime, AnimationCurve curve){
+			startMoveTime = Time.time;
+			endMoveTime = Time.time + moveTime;
+			startMovePos = transform.position;
+			endMovePos = endPos;
+			moveCurve = curve;
+		}
+	}
+}
