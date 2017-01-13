2d9a44b7ca32320efa0d637cf33518136b33ccfc
tsarachan <tes353@nyu.edu>
2017-01-13 11:25:45 -0500

    Adding scoring
    Creating architecture for score system.

---
+++ 3D Hot Potato/Assets/Scripts/Unchanged/ScoreManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ScoreManager : MonoBehaviour {
+
+
+	private Dictionary<string, ScoreCategory> scoreCategories = new Dictionary<string, ScoreCategory>();
+
+
+
+	private class ScoreCategory {
+		public int P1Value { get; set; }
+		public int P2Value { get; set; }
+		private int goodValue = 0;
+
+		public ScoreCategory(int goodValue){
+			this.goodValue = goodValue;
+		}
+	}
+}
