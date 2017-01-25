4ad5efedbcb899950d6eb19b33b796c953f141b1
tsarachan <tes353@nyu.edu>
2017-01-25 17:51:35 -0500

    Assets
    Pushing the assets for other changes. Warning: large amount of stuff.

---
+++ 3D Hot Potato/Assets/Scripts/HoldPower.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * 
+ * Base class for hold powers.
+ * 
+ */
+using System.Collections;
+using UnityEngine;
+
+public abstract class HoldPower : MonoBehaviour {
+
+
+	//tunable variables
+	public float holdDuration = 10.0f; //how long the player has to hold the battery star to trigger the hold power
+
+
+	//internal variables
+	protected PlayerBallInteraction ballScript;
+	protected float holdTimer = 0.0f; //how long this player has been holding the battery star
+
+
+	//initialize variables
+	protected virtual void Start(){
+		ballScript = GetComponent<PlayerBallInteraction>();
+	}
+
+
+	//keep track of how long the player has been holding the ball; activate the hold power when it's been long enough
+	protected virtual void Update(){
+		holdTimer = RunHoldTimer();
+
+		if (holdTimer > holdDuration){
+			ActivateHoldPower();
+		}
+	}
+
+
+	//returns the new value for the hold timer. If the player isn't holding the ball, returns zero.
+	protected float RunHoldTimer(){
+		if (!ballScript.BallCarrier){
+			return 0.0f;
+		} else {
+			float temp = holdTimer;
+
+			temp += Time.deltaTime; 
+
+			return temp;
+		}
+	}
+
+
+	//override this to establish what happens when the player's hold power goes off
+	protected abstract void ActivateHoldPower();
+}
---
+++ 3D Hot Potato/Assets/Scripts/HoldPowerConstructs.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class HoldPowerConstructs : HoldPower {
+
+	//tunable variables
+	public float timeBetweenConstructs = 0.2f;
+	public float offsetAngle = 20.0f; //affects new constructs' direction
+
+
+	//the gameobject that this player is going to send out 
+	private GameObject construct;
+	private const string CONSTRUCT_NAME = "Hold power construct";
+
+
+	//internal variables
+	private float constructTimer = 0.0f;
+	private float lastAngle = 0.0f;
+
+
+	protected override void Update(){
+		holdTimer = RunHoldTimer();
+
+		if (holdTimer > holdDuration){
+			constructTimer += Time.deltaTime;
+
+			if (constructTimer >= timeBetweenConstructs){
+				ActivateHoldPower();
+				constructTimer = 0.0f;
+			}
+		}
+	}
+
+
+	protected override void ActivateHoldPower(){
+		if (lastAngle > 0){
+			lastAngle = lastAngle - 180.0f + offsetAngle;
+		} else {
+			lastAngle = lastAngle + 180.0f + offsetAngle;
+		}
+
+
+		GameObject construct = ObjectPooling.ObjectPool.GetObj(CONSTRUCT_NAME);
+		construct.transform.position = transform.position;
+		construct.GetComponent<ParticleCubeBehavior>().SetDirection(new Vector3(0.0f, lastAngle, 0.0f));
+	}
+}
