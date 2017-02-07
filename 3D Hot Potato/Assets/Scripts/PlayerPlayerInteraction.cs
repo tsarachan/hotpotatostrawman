259c70689a8f89a3aaedd1c45ca51c2b6f2658e9
tsarachan <tes353@nyu.edu>
2017-02-07 16:39:21 -0500

    Bounce players off each other
    Players now bounce off each other on contact.

---
+++ 3D Hot Potato/Assets/Scripts/PlayerPlayerInteraction.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerPlayerInteraction : MonoBehaviour {
+
+	private const string PLAYER_TAG = "Player";
+
+	public float bounce = 20.0f;
+
+	private void OnCollisionEnter(Collision collision){
+		if (collision.gameObject.tag == PLAYER_TAG){
+			collision.gameObject.GetComponent<Rigidbody>().AddExplosionForce(bounce,
+				collision.contacts[0].point,
+				1.0f);
+			GetComponent<Rigidbody>().AddExplosionForce(bounce,
+				collision.contacts[0].point,
+				1.0f);
+		}
+	}
+}
