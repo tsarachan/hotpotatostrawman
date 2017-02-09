b1d9fe0487946a942cbc0655b68cab3d178a10e3
tsarachan <tes353@nyu.edu>
2017-02-09 17:05:24 -0500

    Spark when players collide
    Added small spark when players run into each other.

--- 3D Hot Potato/Assets/Scripts/PlayerPlayerInteraction.cs
+++ 3D Hot Potato/Assets/Scripts/PlayerPlayerInteraction.cs
@@ -3,9 +3,13 @@ using UnityEngine;
 
 public class PlayerPlayerInteraction : MonoBehaviour {
 
-	private const string PLAYER_TAG = "Player";
+	//----------Tunable variables----------
+	public float bounce = 20.0f; //the strength of the force exerted on each player when they collide
 
-	public float bounce = 20.0f;
+
+	//----------Internal variables----------
+	private const string PLAYER_TAG = "Player";
+	private const string COLLISION_PARTICLE = "Spark prefab";
 
 	private void OnCollisionEnter(Collision collision){
 		if (collision.gameObject.tag == PLAYER_TAG){
@@ -15,6 +19,9 @@ public class PlayerPlayerInteraction : MonoBehaviour {
 			GetComponent<Rigidbody>().AddExplosionForce(bounce,
 				collision.contacts[0].point,
 				1.0f);
+
+			GameObject particle = ObjectPooling.ObjectPool.GetObj(COLLISION_PARTICLE);
+			particle.transform.position = collision.contacts[0].point;
 		}
 	}
 }
