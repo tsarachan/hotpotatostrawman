b6832da970f32ddde3f60dc0b4a6101479504b06
tsarachan <tes353@nyu.edu>
2017-02-15 00:13:44 -0500

    Enabled title scene
    Title scene now loads game on any input except R (restart) and Esc (quit).

---
+++ 3D Hot Potato/Assets/Scripts/TitleSceneBehavior.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleSceneBehavior : MonoBehaviour {
+
+	private const string GAME_SCENE = "Level 1";
+
+	private void Update(){
+		if (Input.anyKeyDown){
+			SceneManager.LoadScene(GAME_SCENE);
+		}
+	}
+}
