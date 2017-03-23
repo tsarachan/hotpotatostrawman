e33a8b102991512349309ae4ea91f6d020f10e12
dannynanni <dannynanni.us@gmail.com>
2017-03-23 13:22:33 -0400

    TitleScene Work

---
+++ 3D Hot Potato/Assets/Scripts/BuildingMoveTitle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingMoveTitle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		GameObject managers = GameObject.Find("Managers");
+		InputManager input = (InputManager) managers.GetComponent(typeof(InputManager));
+		input.StartGame();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
