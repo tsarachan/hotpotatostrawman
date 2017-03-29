bb1b4f3b0bbe3d7eb19893ebae3db0e2ae4fca3d
tsarachan <tes353@nyu.edu>
2017-03-29 14:58:38 -0400

    Magnetizing boss grabs alternating players
    Magnetizing boss tries to grab player 1, then switches to player 2 and tries to grab p2, then switches back to player 1, etc.

---
+++ 3D Hot Potato/Assets/Scripts/MagneticArmBehavior.cs
@@ -0,0 +1,34 @@
+﻿namespace MagnetizingBoss
+{
+	using System.Collections;
+	using System.Collections.Generic;
+	using System.Linq;
+	using UnityEngine;
+
+	public class MagneticArmBehavior : MonoBehaviour {
+
+		private MagnetizingBossBehavior bossScript;
+
+
+		//used to determine whether this arm has contacted the player it's attracting
+		private char myTarget = 'z';
+		private const string PLAYER_TAG = "Player";
+
+
+		private void Start(){
+			bossScript = transform.parent.parent.GetComponent<MagnetizingBossBehavior>();
+			myTarget = gameObject.name[1];
+		}
+
+
+		private void OnCollisionEnter(Collision collision){
+			if(collision.gameObject.tag == PLAYER_TAG){
+				if (collision.gameObject.name.Last() == myTarget){
+					collision.gameObject.GetComponent<PlayerEnemyInteraction>().LoseTheGame();
+				} else {
+					bossScript.PlayerBlock(myTarget);
+				}
+			}
+		}
+	}
+}
--- 3D Hot Potato/Assets/Scripts/MagnetizingBossBehavior.cs
+++ 3D Hot Potato/Assets/Scripts/MagnetizingBossBehavior.cs
@@ -11,6 +11,9 @@
 		public float pullStrength = 10.0f; //the strength of the magnetizing force
 		public float armRotSpeed = 10.0f;
 
+		//how many degrees off the arms can be before they snap to a player and start magnetizing
+		public float quaternionMatchTolerance = 10.0f;
+
 
 		//----------Internal variables----------
 
@@ -48,11 +51,26 @@
 			p2Body = player2.GetComponent<Rigidbody>();
 			armAxis = transform.Find(ARM_AXIS_OBJ);
 			rotationTarget = transform.Find(ROTATION_TARGET_OBJ);
-			currentAction = GrabPlayer1;
+			currentAction = PointTowardPlayer1;
 		}
 
 
 		private void Update(){
+			switch (currentStage){
+				case Stage.GrabP1:
+					currentAction = GrabPlayer1;
+					break;
+				case Stage.GrabP2:
+					currentAction = GrabPlayer2;
+					break;
+				case Stage.SwitchToP1:
+					currentAction = PointTowardPlayer1;
+					break;
+				case Stage.SwitchToP2:
+					currentAction = PointTowardPlayer2;
+					break;
+			}
+
 			currentAction();
 		}
 
@@ -62,10 +80,21 @@
 			p1Body.AddForce(forceTowardBoss * pullStrength, ForceMode.Force);
 
 			Vector3 dirTowardP1 = (player1.transform.position - transform.position).normalized * -1;
+	
+			armAxis.rotation = Quaternion.LookRotation(dirTowardP1);
+		}
+
+
+		private void PointTowardPlayer1(){
+			Vector3 dirTowardP1 = (player1.transform.position - transform.position).normalized * -1;
 			rotationTarget.rotation = Quaternion.LookRotation(dirTowardP1);
 			armAxis.rotation = Quaternion.RotateTowards(armAxis.rotation,
 														rotationTarget.rotation,
 														armRotSpeed * Time.deltaTime);
+
+			if (Quaternion.Angle(armAxis.rotation, rotationTarget.rotation) < quaternionMatchTolerance){
+				currentStage = Stage.GrabP1;
+			}
 		}
 
 
@@ -73,10 +102,30 @@
 			Vector3 forceTowardBoss = (transform.position - player2.transform.position).normalized;
 			p2Body.AddForce(forceTowardBoss * pullStrength, ForceMode.Force);
 
-			Vector3 dirTowardP2 = (transform.position - player2.transform.position).normalized;
+			Vector3 dirTowardP2 = Vector3.Cross((player2.transform.position - transform.position).normalized, Vector3.up);
+			armAxis.rotation = Quaternion.LookRotation(dirTowardP2);
+		}
+
+
+		private void PointTowardPlayer2(){
+			Vector3 dirTowardP2 = Vector3.Cross((player2.transform.position - transform.position).normalized, Vector3.up);
+			rotationTarget.rotation = Quaternion.LookRotation(dirTowardP2);
 			armAxis.rotation = Quaternion.RotateTowards(armAxis.rotation,
-														Quaternion.Euler(dirTowardP2),
-														armRotSpeed);
+														rotationTarget.rotation,
+														armRotSpeed * Time.deltaTime);
+
+			if (Quaternion.Angle(armAxis.rotation, rotationTarget.rotation) < quaternionMatchTolerance){
+				currentStage = Stage.GrabP2;
+			}
+		}
+
+
+		public void PlayerBlock(char arm){
+			if (currentStage == Stage.GrabP1 && arm == '1'){
+				currentStage = Stage.SwitchToP2;
+			} else if (currentStage == Stage.GrabP2 && arm == '2'){
+				currentStage = Stage.SwitchToP1;
+			}
 		}
 	}
 }
