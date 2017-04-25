7f29f78c29dbb7de4d5b05f10428568e310f27f7
tsarachan <tes353@nyu.edu>
2017-04-25 11:11:27 -0400

    Resolving missing behavior
    Changes to resolve a missing behavior reference, and to make the title scene environment move.

---
+++ 3D Hot Potato/Assets/Scripts/Title scene/TitleSceneBallBehavior.cs
@@ -0,0 +1,17 @@
+﻿namespace TitleScene
+{
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+
+	public class TitleSceneBallBehavior : MonoBehaviour {
+
+		private const string PLAYER_1_OBJ = "Player 1";
+		private const string CYCLE_OBJ = "Cycle and rider";
+
+
+		private void Start(){
+			transform.parent = GameObject.Find(PLAYER_1_OBJ).transform.Find(CYCLE_OBJ);
+		}
+	}
+}
---
+++ 3D Hot Potato/Assets/Scripts/Title scene/TitleSceneEnvironmentMove.cs
@@ -0,0 +1,41 @@
+﻿namespace TitleScene
+{
+	using UnityEngine;
+	using System.Collections;
+
+	public class TitleSceneEnvironmentMove : MonoBehaviour {
+
+		public float speed = 50.0f;
+		public Vector3 startPos = new Vector3(-34.9f, 9.04f, 74.8f);
+		public float resetZ = -25.0f; //the z-position where this object will cycle back to the front
+
+
+		protected float currentSpeed = 0.0f;
+		public AnimationCurve startMovingCurve;
+		protected float timer = 0.0f;
+		public float timeToFullSpeed = 1.0f;
+
+
+		protected virtual void Update(){
+			currentSpeed = GetCurrentSpeed();
+
+			transform.localPosition -= Vector3.forward * currentSpeed * Time.deltaTime;
+
+			if (transform.localPosition.z <= resetZ) {
+
+				transform.localPosition = startPos;
+			}
+		}
+
+
+		protected float GetCurrentSpeed(){
+			if (timer >= timeToFullSpeed){
+				return speed;
+			} else {
+				timer += Time.deltaTime;
+
+				return Mathf.Lerp(0.0f, speed, startMovingCurve.Evaluate(timer/timeToFullSpeed));
+			}
+		}
+	}
+}
